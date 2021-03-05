651ff9acc7a9e0c7d2b9b1d085c8bb6366d8403c
shajidur <shajidurrahmansaad10@gamil.com>
2021-03-05 18:07:26 +0600

    added new

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const mynamelength = 20;
-console.log(mynamelength)
+console.log("hello world");
+
+let name = "Shajidur rahman";
+
+let person = {
+    name1 : "shjidur rahman",
+    age : 13
+
+};
+
+
+let firstnum = prompt("first number: ");
+let secondnum = prompt("second number: ");
+
+document.getElementById("htmljs").innerHTML = firstnum * secondnum;
+
